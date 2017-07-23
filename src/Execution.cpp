f1d62997511dbc0c4f43476fff286b3f0bc2b1e3
Loïc P <loic.pirez@epitech.eu>
2017-07-23 15:36:31 +0200

    Fix regex for 0 numbers

--- src/Execution.cpp
+++ src/Execution.cpp
@@ -61,7 +61,7 @@ void Execution::extractInformationsFromLine(std::string line) {
     Instructions *i = new Instructions;
     std::regex s("(pop|clear|dup|swap|dump|add|sub|mul|div|mod|print|exit)");
     std::regex v(
-            "((load|store|push|assert)\\s(int8|int16|int32|float|double|bigdecimal)\\((-?[1-9]\\d*(\\.\\d+)?)\\))");
+            "((load|store|push|assert)\\s(int8|int16|int32|float|double|bigdecimal)\\((-?[0-9]\\d*(\\.\\d+)?)\\))");
     std::smatch match;
     try {
         if (std::regex_match(line, s)) {
